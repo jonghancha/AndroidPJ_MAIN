bbce697baeb10f16e4805f2aa6f60c8a10222af4
ParkJiEun <qkrwldms011@naver.com>
2021-01-07 15:21:31 +0900

    21/01/07 플로팅 버튼 완성 - 메인

--- AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/Frmt_Home.java
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/Frmt_Home.java
@@ -3,19 +3,18 @@ package com.android.androidpj_main.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.android.androidpj_main.R;
-import com.android.androidpj_main.Lip.LipMainActivity;
+import com.android.androidpj_main.Test.TestMainActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Frmt_Home extends Fragment {
@@ -25,11 +24,19 @@ public class Frmt_Home extends Fragment {
 
 
     // 21.01.07 지은 추가 ---------------
-    FloatingActionButton fabMain, fabMake, fabHoney;
-    Float translationY = 100f;
-    Boolean isMenuOpen = false;
-    OvershootInterpolator interpolator = new OvershootInterpolator();
+
+    //boolean flag to know if main FAB is in open or closed state.
+    private boolean fabExpanded = false;
+    private FloatingActionButton fabMain;   // 메안 플로팅 버튼
+    private FloatingActionButton fabMake, fab_honey;
+
+    //Linear layout holding the Save submenu
+    private LinearLayout layoutFabSave;
+    //Linear layout holding the Edit submenu
+    private LinearLayout layoutFabEdit;
     //------------------------
+
+    Button gotest;
 
 
 
@@ -43,10 +50,44 @@ public class Frmt_Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.frmt_home,container,false);
 
-        //플로팅 버튼 관련
-        initFabMenu();
+        //플로팅 버튼 관련 - xml 아이디 선언
+        initUI();
 
+        gotest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TestMainActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        fabMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (fabExpanded == true) {
+                    closeSubMenusFab();
+                } else {
+                    openSubMenusFab();
+                }
+            }
+        });
+
+        fab_honey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "꿀팁 차차", Toast.LENGTH_SHORT).show();
+            }
+        });
+        fabMake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "메이크업 영상", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        //Only main FAB is visible in the beginning
+        closeSubMenusFab();
         return v;
     }
 
@@ -55,75 +96,33 @@ public class Frmt_Home extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-
-
-//21.01.07 지은 추가------------------------
-    //플로팅 버튼 관련
-    private void initFabMenu() {
-        fabMain = v.findViewById(R.id.fabMain);         // 숨겼다가 띄우는 fab
-        fabMake = v.findViewById(R.id.fabMake);         // 메이크업 영상 fab
-        fabHoney = v.findViewById(R.id.fabHoney);       // 꿀팁 fab
-
-        fabMake.setAlpha(0f);
-        fabHoney.setAlpha(0f);
-
-        fabMake.setTranslationY(translationY);
-        fabHoney.setTranslationY(translationY);
-
-        fabMain.setOnClickListener(flCliclListener);
-        fabMake.setOnClickListener(flCliclListener);
-        fabHoney.setOnClickListener(flCliclListener);
+    // 버튼선언 - xml 과의
+    private void initUI() {
+        gotest = v.findViewById(R.id.btn_test);
+        fabMain = v.findViewById(R.id.fabMain);
+        fabMake = v.findViewById(R.id.fabMake);
+        fab_honey = v.findViewById(R.id.fab_honey);
+        layoutFabSave = v.findViewById(R.id.layoutFabSave);
+        layoutFabEdit = v.findViewById(R.id.layoutFabEdit);
     }
-    // 메인 fab 클릭시 fab 띄우기
-    private void openMenu() {
-        isMenuOpen = !isMenuOpen;
 
-        fabMain.animate().setInterpolator(interpolator).rotation(45f).setDuration(300).start();
-
-        fabMake.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
-        fabHoney.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
-
-
+    //closes FAB submenus
+    private void closeSubMenusFab() {
+        layoutFabSave.setVisibility(View.INVISIBLE);
+        layoutFabEdit.setVisibility(View.INVISIBLE);
+        fabMain.setImageResource(R.drawable.ic_add_black_24dp);
+        fabExpanded = false;
     }
-    // 메인 fab 클릭시 띄워져있는데 fab 숨기기
-    private void closeMenu() {
-        isMenuOpen = !isMenuOpen;
 
-        fabMain.animate().setInterpolator(interpolator).rotation(0f).setDuration(300).start();
-
-        fabMake.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
-        fabHoney.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
-
+    //Opens FAB submenus
+    private void openSubMenusFab() {
+        layoutFabSave.setVisibility(View.VISIBLE);
+        layoutFabEdit.setVisibility(View.VISIBLE);
+        //'X' 아이콘으로 변경
+        fabMain.setImageResource(R.drawable.ic_close);
+        fabExpanded = true;
     }
-    // 클릭했을 때 일어나는 이벤트
-    View.OnClickListener flCliclListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.fabMain:
-                    Log.v(TAG, "onClick: fab main");
-                    if (isMenuOpen) {
-                        closeMenu();
-                    } else {
-                        openMenu();
-                    }
-                    break;
-                case R.id.fabMake:
-                    Intent intentM = new Intent(getActivity(), LipMainActivity.class);
-                    startActivity(intentM);
-                    Toast.makeText(getContext(), "메이크업 영상 보러가기 클릭", Toast.LENGTH_SHORT).show();
-                    Log.v(TAG, "onClick: fab makeup");
-                    break;
-                case R.id.fabHoney:
-                    Intent intentH = new Intent(getActivity(), HoneyTipActivity.class);
-                    startActivity(intentH);
-                    Toast.makeText(getContext(), "꿀팁 차차 보러가기 클릭", Toast.LENGTH_SHORT).show();
-                    Log.v(TAG, "onClick: fab honey");
-                    break;
-            }
 
-        }
-    };
-//--------------------------
+
 
 }//-----------
