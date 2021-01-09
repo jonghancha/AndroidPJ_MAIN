8e4f7e09c6f1fd293e3db141e976807837ad697e
imkanghooo <imkanghoo@icloud.com>
2021-01-10 03:39:05 +0900

    LoginActivity - 해시 키 추가 activity_login.xml 완료 activity_findpw.xml 완료

--- AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/LoginActivity.java
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/LoginActivity.java
@@ -2,11 +2,18 @@ package com.android.androidpj_main.Activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.android.androidpj_main.R;
+
+import java.security.MessageDigest;
 
 
 public class LoginActivity extends Activity {
@@ -25,6 +32,10 @@ public class LoginActivity extends Activity {
         gomain.setOnClickListener(gogo);
 
 
+    //--------------------------------------------
+        getAppKeyHash(); //해시 키.
+    //--------------------------------------------
+
     }
 
     View.OnClickListener gogo = new View.OnClickListener() {
@@ -37,5 +48,23 @@ public class LoginActivity extends Activity {
     };
 
 
+        //-------- 해시 키 찾는 코드 -----------------------------------------------------------------------
+        private void getAppKeyHash() {
+            try {
+                PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+                for (Signature signature : info.signatures) {
+                    MessageDigest md;
+                    md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    String something = new String(Base64.encode(md.digest(), 0));
+
+                    Log.e(TAG,"Hash key : " + something);
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                Log.e(TAG,"name not found" + e.toString());
+            }
+        }
+        //----------------------------------------------------------------------------------------------
 
 }//-------------------
