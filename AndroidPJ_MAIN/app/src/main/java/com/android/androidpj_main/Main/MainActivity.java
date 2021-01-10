a4733ff73e5d9d9ec3b6b02b85f95457e90be594
jonghancha <jonghancha@gmail.com>
2021-01-11 03:02:09 +0900

    main 광고, 다시보지 않기

--- AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Main/MainActivity.java
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Main/MainActivity.java
@@ -2,8 +2,11 @@ package com.android.androidpj_main.Main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
@@ -17,6 +20,9 @@ import com.android.androidpj_main.Activity.SearchActivity;
 import com.android.androidpj_main.Adapter.ViewPageAdapter;
 import com.android.androidpj_main.R;
 import com.google.android.material.tabs.TabLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +40,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     //****************************************
+
+    // 종한 광고 추가
+    // Debug tag, for logging
+    static final String TAG = "Main";
+
+    // SharedPreferences 정의
+    private SharedPreferences SPreferences;
+
+    // SharedPreferences 접근 이름, 저장 데이터 초기화
+    private final String NameSPreferences = "Day";
+    private String strSDFormatDay = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +91,28 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setElevation(0);
         //****************************************
+
+        // '오늘 그만 보기' 기능을 위한 날짜 획득
+        long CurrentTime = System.currentTimeMillis(); // 현재 시간을 msec 단위로 얻음
+        Date TodayDate = new Date(CurrentTime); // 현재 시간 Date 변수에 저장
+        SimpleDateFormat SDFormat = new SimpleDateFormat("dd");
+        strSDFormatDay = SDFormat.format(TodayDate); // 'dd' 형태로 포맷 변경
+
+        // SharedPreferences 획득
+        SPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String strSPreferencesDay = SPreferences.getString(NameSPreferences, "0");
+        Log.v(TAG, strSPreferencesDay);
+
+        // 광고 띄움
+        // 오늘날짜 - 이전 날짜
+        if((Integer.parseInt(strSDFormatDay) - Integer.parseInt(strSPreferencesDay)) != 0)
+            StartMainAdDialog();
+    }
+
+    // 초기 실행시 광고 Dialog
+    public void StartMainAdDialog() {
+        MyDialogFragment myDialogFragment = new MyDialogFragment();
+        myDialogFragment.show(getSupportFragmentManager(), "MyFragment");
     }
 
 
