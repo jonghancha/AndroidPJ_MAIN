e23fe9ae3d503351d76182858604765b6a42a2b5
ParkSeMi <qkrtpa12@naver.com>
2021-01-14 01:39:47 +0900

    21.01.13 세미 추가, 추천상품 userColor 받아서 추천상품 띄우기

--- AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Share/ShareVar.java
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Share/ShareVar.java
@@ -3,13 +3,8 @@ package com.android.androidpj_main.Share;
 public class ShareVar {
     // 공통 ip ************************************************
 
-<<<<<<< HEAD
-    public final static String macIP = "192.168.2.3";
+    public final static String macIP = "192.168.43.159";
     // 설정 ***************************************************
-=======
-    public final static String macIP = "192.168.43.220";
-
->>>>>>> 9b13f98775103ad6e7a865666ace71772bb46f3c
 
 
 }
