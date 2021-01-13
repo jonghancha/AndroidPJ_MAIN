3dc49b39d751a628f14ab8af05cc1387807aec96
jonghancha <jonghancha@gmail.com>
2021-01-14 00:11:12 +0900

    21.01.14  종한 : 장바구니 총 주문가격변경

--- AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/CartActivity.java
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/CartActivity.java
@@ -27,7 +27,7 @@ import com.android.androidpj_main.Share.ShareVar;
 
 import java.util.ArrayList;
 
-public class CartActivity extends AppCompatActivity {
+public class CartActivity extends AppCompatActivity implements OnChangeCheckedPrice{
 
     final static String TAG = "CartActivity";
     String urlAddr = null;
@@ -54,6 +54,7 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
 
         setTitle("장바구니");
 
@@ -107,7 +108,17 @@ public class CartActivity extends AppCompatActivity {
             }
         });
         connectGetCart();
+
+//        cartAdapter.setOnChangeCheckedPrice(new OnChangeCheckedPrice() {
+//            @Override
+//            public void changedPrice(int totalPrice) {
+//                Log.v(TAG, "**메인 가격변경 리스너 들어옴 **");
+//                btnCartToOrder.setText("총" + totalPrice + "원 주문하기");
+//            }
+//        });
     }
+
+
 
     // 상품을 띄우는
     private void connectGetCart() {
@@ -120,7 +131,7 @@ public class CartActivity extends AppCompatActivity {
             //gridLayoutManager = new GridLayoutManager(this, 2);
             cart = (ArrayList<Cart>) object;
 
-            cartAdapter = new CartAdapter(CartActivity.this, R.layout.item_cart, cart);
+            cartAdapter = new CartAdapter(CartActivity.this, R.layout.item_cart, cart, this);
             cartRecyclerView.setAdapter(cartAdapter);
             cartRecyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
             layoutManager = new LinearLayoutManager(CartActivity.this);
@@ -141,4 +152,10 @@ public class CartActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void changedPrice(int totalPrice) {
+        Log.v(TAG, "**메인 가격변경 리스너 들어옴 **");
+        btnCartToOrder.setText("총" + totalPrice + "원 주문하기");
+
+    }
 }
