1e244def750ffb73af727c465447940e996f52b4
jonghancha <jonghancha@gmail.com>
2021-01-14 23:03:20 +0900

    21.01.14 충돌..?

--- AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Share/ShareVar.java
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Share/ShareVar.java
@@ -3,7 +3,7 @@ package com.android.androidpj_main.Share;
 public class ShareVar {
     // 공통 ip ************************************************
 
-    public final static String macIP = "192.168.2.18";
+    public final static String macIP = "112.170.44.211";
 
 
 
