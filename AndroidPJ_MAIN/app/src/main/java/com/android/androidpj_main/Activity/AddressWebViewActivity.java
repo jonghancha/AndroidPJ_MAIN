0271ae85b943a1897d513819806268bebed81b56
차종한 <jonghancha@gmail.com>
2021-01-14 19:02:58 +0900

    21.01.14 종한 : 도로명주소 하는중

---
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/AddressWebViewActivity.java
@@ -0,0 +1,53 @@
+package com.android.androidpj_main.Activity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.androidpj_main.R;
+import com.android.androidpj_main.Share.ShareVar;
+
+public class AddressWebViewActivity extends AppCompatActivity {
+
+    private WebView browser;
+    String macIP = ShareVar.macIP;
+
+    class MyJavaScriptInterface
+    {
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processDATA(String data) {
+            Bundle extra = new Bundle();
+            Intent intent = new Intent();
+            extra.putString("data", data);
+            intent.putExtras(extra);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_web_view);
+
+        browser = (WebView) findViewById(R.id.webView);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.addJavascriptInterface(new MyJavaScriptInterface(), "Android");
+
+        browser.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                browser.loadUrl("javascript:sample2_execDaumPostcode();");
+            }
+        });
+
+        browser.loadUrl("http://" + macIP + ":8080/test/daum.html");
+    }
+}
