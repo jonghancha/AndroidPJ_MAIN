0146cd0481c172238b9d706b9c5e406aee7a3592
차종한 <jonghancha@gmail.com>
2021-01-14 16:43:48 +0900

    21.01.14 종한 : 주문/결제 창 마무리, -> 배송지 등록 창 시작

--- AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/RegisterAddress.java
+++ AndroidPJ_MAIN/app/src/main/java/com/android/androidpj_main/Activity/RegisterAddress.java
@@ -1,16 +1,20 @@
 package com.android.androidpj_main.Activity;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
 
 import com.android.androidpj_main.R;
 
-public class MyUpdateActivity extends AppCompatActivity {
+public class RegisterAddress extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_myupdate);
+        setContentView(R.layout.activity_register_address);
+
+        // 타이틀
+        setTitle("배송지 등록");
     }
 }
